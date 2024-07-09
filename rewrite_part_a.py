59d850b65dc3e2022f44311ce6ade2349bf90274
Mingrong Hou <Mingrong.Hou@uftwf.org>
2024-07-09 16:16:17 -0400

    added rewrite_part_a and its new output json file

--- rewrite_part_a.py
+++ rewrite_part_a.py
@@ -2,43 +2,24 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
-from selenium.common.exceptions import NoSuchElementException
 from urllib.parse import urlparse
 # from geopy.geocoders import Here #EMFSMes4qAjPG6GIaFqtAt8DN_-Dh0KeqV-7zgdrmSU
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderUnavailable
 from retrying import retry
 from word2number import w2n
-
 import re
-import folium
+import requests
+
 start = time.time()
 
 driver = webdriver.Chrome()
 driver.get("https://schoolsearch.schools.nyc/")
 
-# Locate by using the attribute set for this button,
-# click the search button
-# wait for the new content
+# Locate the search button, click the search button so the website starts to fetch API info
 search_button = driver.find_element(By.XPATH, '//input[@type="submit" and @class="btn btn-primary" and @value="Search"]')
 search_button.click()
 time.sleep(5)
-
-# Locate the inner scrollable container
-inner_container = driver.find_element(By.CSS_SELECTOR, '.iScroll')
-
-# Set the current height of the inner scroller 
-# In a loop, keep scrolling the inner scroller and update the newest height,
-#               compare between the currect and the next height to find out if scroller reach its end
-#               if yes, break the loop; else, continue until yes
-last_height = driver.execute_script("return arguments[0].scrollHeight", inner_container)
-while True:
-    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", inner_container)
-    time.sleep(2)
-    new_height = driver.execute_script("return arguments[0].scrollHeight", inner_container)
-    if new_height == last_height:
-        break
-    last_height = new_height
 
 # Now, we have the html content of elements inside the inner scroller
 # insert all the info (school name, lat, lng, DOE url) into the dictionary of dictionaries
@@ -47,6 +28,7 @@ no_url = set() # store a list of school that return None when trying to locate i
 all_links = set() #get all links from the first layer
 address_issue_schools = set() # get the name of all schools that have unformatted address that unable to convert to coordinates
 school_name_issue_urls = set() #doe link is raise error of school name not found
+google_url = set() #
 
 def add_numeric_id(street):
     street_Ones = int(street) % 10
@@ -94,7 +76,6 @@ def convert_word_to_numeric(word):
             "Twenty-Third": "23rd",
             "Twenty-Fourth": "24th",
             "Twenty-Fifth": "25th",
-            # Add more ordinal conversions as needed
         }
         if word.endswith(("st", "nd", "rd", "th")):
             if word in ordinal_mapping:
@@ -109,6 +90,7 @@ def convert_word_to_numeric(word):
     except ValueError:
         return False
 
+# function that format address and return coordinate pairs
 def format_address(geolocator, address):
     # Case 1 : 511 7 Ave, Brooklyn, NY 11215, 8-21 Bay 25 Street, Queens, NY 11691  -- 7 Avenue to 7th Avenue, detect Bay as direction
     # Case 2 : 10 South Street, Slip 7, Manhattan, NY 10004                         -- make sure South is detected under streetname and 'Slip 7' is removed
@@ -209,135 +191,94 @@ def geocode_with_retry(geolocator, location):
         print(f"GeocoderUnavailable: {e}")
         raise
 
-def find_element_with_retry(driver, by, value, max_retries=5, delay=2):
-    retries = 0
-    while retries < max_retries:
-        try:
-            element = driver.find_element(by, value)
-            return element
-        except NoSuchElementException:
-            retries += 1
-            print(f"Element not found, retrying ({retries}/{max_retries})...")
-            time.sleep(delay)
-    raise NoSuchElementException(f"Element not found after {max_retries} retries")
+# get each address and format address then store 
+URL = "https://ws.schools.nyc/schooldata/GetSchools?search=&borough=&grade="
+r = requests.get(url = URL)
+data = r.json() # list of dictionories
+driver.quit()
 
-# insert (school_url, grades, district, borough) into the specific dictionary of each school
-# input: the url of each school for second layer crawling
-#        dictionary that we store the info that we got from each school
-def website_crawler(url):
-    try:
-        driver.get(url)
-        # The school DOE link would return 404 error thus hard code its info
-        # American Dream Charter School II
-        # Imagine Early Learning Center @ City College - MBVK
+# In total there is 2900 schools
+# loop through each school and store all the info:
+# {'locationCode': 'K001', 'type': 'DOE', 'boroughName': 'Brooklyn', 'boroughCode': 'K', 'name': 'P.S. 001 The Bergen', 
+# 'phoneNumber': '718-567-7661', 'primaryAddressLine': '309 47 STREET', 'zip': '11220', 'grades': 'PK,0K,01,02,03,04,05,SE', 
+# 'stateCode': 'NY', 'x': '-8238913.43780000', 'y': '4960699.12320000', 'profile': '', 'neighborhood': 'Sunset Park West                                                           ', 
+# 'district': '15', 'distance': '', 'dataflag': 'L'}
+# 'profile' contains the school's private url link, need to turn lower case
+avg_loop_runtime = 0
+for school in data:
+    each_school_time_avg = time.time()
+    school_dict = school_item_dict.setdefault(school['name'].strip(), {})
+
+    #coordinates
+    address = school['primaryAddressLine'].lower() + ', ' + school['boroughName'] + ', ' + school['stateCode'] + ' ' + school['zip']
+    geolocator = Nominatim(user_agent="my_request")
+    address_x = address.strip()
+    print(address_x)
+
+    loc = format_address(geolocator, address_x)
+    location = geocode_with_retry(geolocator, loc)
+    if location != None:
+        print("Got location: " + location.address + "\n")
+        school_dict["Latitude"] = location.latitude #1st pair
+        school_dict["Longitude"] = location.longitude  #2nd pair
+
+    # Collect the name of all school where its address was not able to convert to coordinate
+    else:
+        school_dict["Latitude"] = "00000000000000000000000" #1st pair
+        school_dict["Longitude"] = "0000000000000000000000000" #2nd pair
+        address_issue_schools.add(school['name'].strip())
     
-        school_name = find_element_with_retry(driver, By.CSS_SELECTOR, 'div.module-header h1.title')
-        # for each item, create a new dictionary with school name as the key if it doesn't exist, key, value
-        school_dict = school_item_dict.setdefault(school_name.text.strip(), {})
+    # Grade store
+    school_dict["Grade"] = school['grades']
 
-        address = driver.find_element(By.CSS_SELECTOR, 'div.module-header a')
-        
-        geolocator = Nominatim(user_agent="my_request")
-        address_x = address.text.split('\n')[0].strip().strip()
-        
-        loc = format_address(geolocator, address_x)
-        location = geocode_with_retry(geolocator, loc)
-        if location != None:
-            print("Got location: " + location.address + "\n")
-            school_dict["Latitude"] = location.latitude #1st pair
-            school_dict["Longitude"] = location.longitude  #2nd pair
+    # District store
+    school_dict["District"] = school['district']
 
-        # Collect the name of all school where its address was not able to convert to coordinate
+    # Borough store
+    school_dict["Borough"] = school['boroughName']
+    
+    # extract the school url using the exact path, if no school website is found, add the school name onto the no url list
+    school_dict["School Website"] = school['profile'].lower()
+    if school['profile'] != '':
+        domain = urlparse(school['profile'].lower()).netloc.replace('www.', '').split('.')
+        if domain == "google": #skip
+            google_url.add(school['name'].strip())
+            school_dict["Domain_1"] = ''
+            school_dict["Domain_2"] = ''
+            school_dict["Domain_3"] = ''
+            school_dict["Domain_4"] = ''
         else:
-            school_dict["Latitude"] = "00000000000000000000000" #1st pair
-            school_dict["Longitude"] = "0000000000000000000000000" #2nd pair
-            address_issue_schools.add(school_name.text.strip())
-            print("NOOOOOOOOOOOOOOOOOOOOOOOOOO\n")
-            print(address_x)
-            print("No coordinate: " + str(address_issue_schools))
-            print("\n\n\n")
-            
-        other_info = driver.find_element(By.CSS_SELECTOR, 'div#tab-panel-01 ul.box-list')
-
-        # Grade store
-        try:
-            grade_span = other_info.find_element(By.XPATH, ".//li[.//strong[text()='Grades:']]//span")
-            school_dict["Grade"] = grade_span.text.split(':')[-1].strip() # 3rd pair
-        except NoSuchElementException:
-            school_dict["Grade"] = "None"
-
-        # District store
-        try: 
-            district_span = other_info.find_element(By.XPATH, ".//li[.//strong[text()='Geographic District:']]//span")
-            school_dict["District"] = district_span.text.split(':')[-1].strip() # 4th pair
-        except NoSuchElementException:
-            school_dict["District"] = "None"
-
-        # Borough store
-        try:
-            borough_span = other_info.find_element(By.XPATH, ".//li[.//strong[text()='Borough:']]//span")
-            school_dict["Borough"] = borough_span.text.split(':')[-1].strip() # 5th pair
-        except NoSuchElementException:
-            school_dict["Borough"] = "None"
-        
-        # extract the school url using the exact path, if no school website is found, add the school name onto the no url list
-        try:   
-            school_url_elements = driver.find_element(By.XPATH, "//a[contains(text(), 'School Website')]")
-            print(school_url_elements)
-            if school_url_elements: # store the website into the dictionary
-                url = school_url_elements.get_attribute("href")
-                school_dict["School Website"] = url # 6th pair
-                domain = urlparse(url).netloc.replace('www.', '').split('.')
-                if domain == "google": #skip
-                    school_dict["Domain_1"] = "None"
-                    school_dict["Domain_2"] = "None"
-                    school_dict["Domain_3"] = "None"
-                    school_dict["Domain_4"] = "None"
-                else:
-                    school_dict["Domain_1"] = domain[0] + ".org"
-                    school_dict["Domain_2"] = domain[0] + ".com"
-                    school_dict["Domain_3"] = domain[0] + ".edu"
-                    school_dict["Domain_4"] = domain[0] + ".net"
-
-        except NoSuchElementException:
-            schools_name = driver.find_element(By.CSS_SELECTOR, 'div.module.school-detail h1.title')
-            no_url.add(schools_name.text.strip())
-            school_dict["School Website"] = "None"
-            school_dict["Domain_1"] = "None"
-            school_dict["Domain_2"] = "None"
-            school_dict["Domain_3"] = "None"
-            school_dict["Domain_4"] = "None"
+            school_dict["Domain_1"] = domain[0] + ".org"
+            school_dict["Domain_2"] = domain[0] + ".com"
+            school_dict["Domain_3"] = domain[0] + ".edu"
+            school_dict["Domain_4"] = domain[0] + ".net"
+    else:
+        no_url.add(school['name'].strip())
+        school_dict["Domain_1"] = ''
+        school_dict["Domain_2"] = ''
+        school_dict["Domain_3"] = ''
+        school_dict["Domain_4"] = ''
     
-    #if doe link is giving the issue of school name not found, add to list for special adjustment
-    except NoSuchElementException:
-        school_name_issue_urls.add(url)
-
-    finally:
-        print(school_item_dict)
-
-# extract all school items in this page at once and store
-schools_item = driver.find_elements(By.XPATH, '//div[@class="item school-item" and @data-lat and @data-lng]')
-for item in schools_item:
-    # grab all the a tag within each school item, which should be one per each
-    a_tag = item.find_element(By.CSS_SELECTOR, 'h2.title a')
-    all_links.add(a_tag.get_attribute('href'))
-
-# call the 2nd layer of crawling to complete storing rest of the info for this school
-# sending in the DOE link and the value which is the inner dictionary
-sorted_links = sorted(all_links)
-for link in sorted_links:
-    website_crawler(link) # doe link does not need to be store into the database, the school's own url will be store
+    end_each = time.time()
+    each_school_time_avg = end_each - each_school_time_avg
+    avg_loop_runtime += each_school_time_avg
 
 print("\nThis is list of schools without url provided in the DOE website(no private school url attached): " + str(no_url))
 print("\nDOE link issue: " + str(school_name_issue_urls))
 print("\nNo coordinate get for address converting: " + str(address_issue_schools))
-json_str = json.dumps(school_item_dict, indent= 4)
+not_converted = len(address_issue_schools)
+percentage = 100 - 100 * float(not_converted)/float(len(data))
+no_url_number = len(no_url)
+print("\nGeocode was not able to convert " + str(not_converted) + " school addresses.\nThus, the converting percentage is " + str(percentage) + "%.")
+print("There are " + str(no_url_number) + " schools with no private url provided.")
+avg_loop_runtime = avg_loop_runtime/len(data)
+print(f"Average loop time: {avg_loop_runtime} seconds")
 
-with open("output.json", "w") as outfile:
+json_str = json.dumps(school_item_dict, indent= 4)
+with open("data_as_json.json", "w") as outfile:
     outfile.write(json_str)
     print("\nSaved")
 
-driver.quit()
 end = time.time()
 runtime = end - start
 print(f"Runtime of the program: {runtime} seconds")
